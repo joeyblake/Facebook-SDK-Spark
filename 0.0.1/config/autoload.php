8754f64e5211a400d1029a1fc919a83ec293cd35
Joey Blake <joeyblake@gmail.com>
2012-01-23 00:08:51 -0500

    initial commit

---
+++ 0.0.1/config/autoload.php
@@ -0,0 +1,10 @@
+<?php
+
+# Load facebook library
+$autoload['libraries'] = array('src/facebook');
+
+# Load the facebook config when the spark is loaded
+$autoload['config'] = array('facebook_config');
+
+# Load the facebook helper when the spark is loaded
+//$autoload['helper'] = array('facebook_config');
---
+++ 0.0.1/config/facebook_config.php
@@ -0,0 +1,8 @@
+<?php 
+
+$config['uri_protocol']    = "PATH_INFO";
+$config['permitted_uri_chars'] = 'a-z 0-9?~%.:_\-';
+$config['enable_query_strings'] = TRUE;
+
+$config['appId'] = '[your appID]';
+$config['secret'] = '[your secret]';
